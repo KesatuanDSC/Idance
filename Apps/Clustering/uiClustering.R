49a2837b59d460a6a5c9b79e5aa9810b7d0be1fa
KesatuanDSC <kdsc.325@gmail.com>
2025-07-08 15:44:28 +0700

    Add files via upload

---
+++ Apps/Clustering/uiClustering.R
@@ -0,0 +1,80 @@
+iClustering_tab <- tabItem(
+  tabName = "iClustering",
+  tabBox(
+    title = "Interactive Clustering",
+    elevation = 2,
+    id = "Box",
+    width = 12,
+    collapsible = FALSE, 
+    closable = FALSE,
+    type = "tabs",
+    status = "warning",
+    solidHeader = TRUE,
+    selected = "Preparation",
+    # Preparation Tab ----
+    tabPanel(
+      "Preparation",
+      box(
+        title = "Preparation",
+        status = "warning",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        width = 12,
+        height = "100%",
+        id = "Box",
+        fileInput("uploadCluster", "Upload a file", accept = c(".csv", ".tsv",".xlsx")),
+        tableOutput("filesCluster"),
+        gt_output("headClustering")
+      )
+    ),
+    # Feature Selection Tab ----
+    tabPanel(
+      "FeatureSelection",
+      box(
+        "FeatureSelection",
+        title = "Features Selection",
+        status = "warning",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        width = 12,
+        height = "100%",
+        id = "Box",
+        selectizeInput(
+          'ColumnClustering', 
+          label = "Choose Features [ >=2 ]", 
+          choices = NULL, 
+          multiple = TRUE,
+          options = list(
+            placeholder = 'Select Features [ >=2 ]'
+          )
+        ),
+        selectizeInput(
+          'IDColumnClustering', 
+          label = "Choose Identity Feature[s] [ >=2 ]", 
+          choices = NULL, 
+          multiple = TRUE,
+          options = list(
+            placeholder = 'Select Identity Features [ >=2 ]'
+          )
+        ),
+        actionButton(inputId = "Clusterize", label = "Show Cluster"),
+        plotlyOutput("ClusterPlot")
+      )
+    ),
+    # Summary Data Tab ----
+    tabPanel(
+      "Data Summary",
+      box(
+        "Data Summary",
+        title = "Data Summary",
+        status = "warning",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        width = 12,
+        height = "100%",
+        id = "Box",
+        verbatimTextOutput("SummaryCluster")
+      )
+    )
+  )
+)
