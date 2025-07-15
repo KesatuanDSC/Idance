068dd4d0347320affe49f62fc38e1f56f6896aba
KesatuanDSC <kdsc.325@gmail.com>
2025-07-15 14:25:46 +0700

    Update uiRegression.R

--- Apps/Regression/uiRegression.R
+++ Apps/Regression/uiRegression.R
@@ -1,60 +1,75 @@
 #' Regression Tab ----
 iRegression_tab <- tabItem(
   tabName = "iRegression",
-  box(
+  tabBox(
     title = "Regression",
-    status = "warning",
+    elevation = 2,
     id = "Box",
-    solidHeader = TRUE,
-    collapsible = TRUE,
     width = 12,
-    height = "100%",
-    fileInput("uploadRegression", "Upload a file", accept = c(".csv", ".tsv",".xlsx")),
-    uiOutput("varSelectUI"),
-    fluidRow(
-      column(
-        width = 6,
-        box(
-          title = "Regression Summary",
-          status = "warning",
-          id = "Box",
-          solidHeader = TRUE,
-          collapsible = TRUE,
-          width = 12,
-          height = "100%",
-          verbatimTextOutput("regSummary") %>% withSpinner(color = "#5bc0de")
-        )
+    collapsible = TRUE, 
+    closable = FALSE,
+    type = "tabs",
+    status = "warning",
+    solidHeader = TRUE,
+    selected = "Regression",
+    # Preparation Tab ----
+    tabPanel(
+      "Regression",
+      box(
+        title = "Regression",
+        status = "warning",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        width = 12,
+        height = "100%",
+        id = "Box",
+        fileInput("uploadRegression", "Upload a file", accept = c(".csv", ".tsv",".xlsx")),
+        uiOutput("varSelectUI")
       ),
-      column(
-        width = 6,
-        box(
-          title = "Regression Plot",
-          status = "warning",
-          id = "Box",
-          solidHeader = TRUE,
-          collapsible = TRUE,
+      fluidRow(
+        column(
           width = 12,
-          height = "100%",
-          plotOutput("regressionPlot") %>% withSpinner(color = "#5bc0de")
+          box(
+            title = "Regression Plot",
+            id = "Box",
+            status = "warning",
+            solidHeader = TRUE,
+            collapsible = TRUE,
+            width = 12,
+            height = "100%",
+            plotlyOutput("regressionPlot") %>% withSpinner(color = "#FFEB7A")
+          )
         )
       )
     ),
-    fluidRow(
-      column(
+    tabPanel(
+      "Data Summary",
+      box(
+        "Data Summary",
+        title = "Data Summary",
+        status = "warning",
+        solidHeader = TRUE,
+        collapsible = TRUE,
         width = 12,
-        box(
-          title = "Explanation",
-          collapsed = TRUE,
-          status = "warning",
-          id = "Box",
-          solidHeader = TRUE,
-          collapsible = TRUE,
-          width = 12,
-          height = "100%",
-          verbatimTextOutput("regExplanation") %>% withSpinner(color = "#5bc0de"),
-          downloadButton("downloadregExplanation", "Download Explanation")
-        )
-      ),
+        height = "100%",
+        id = "Box",
+        verbatimTextOutput("regSummary")
+      )
+    ),
+    tabPanel(
+      "Explanation",
+      box(
+        "Explanation",
+        title = "Explanation",
+        status = "warning",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        width = 12,
+        height = "100%",
+        id = "Box",
+        verbatimTextOutput("regExplanation") %>% withSpinner(color = "#FFEB7A"),
+        downloadButton("downloadregExplanation", "Download Explanation")
+      )
     )
   )
 )
