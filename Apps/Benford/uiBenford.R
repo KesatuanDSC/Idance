01462a234c3abaaed5f9cc1f52862b3243df2c4d
KesatuanDSC <kdsc.325@gmail.com>
2025-07-22 16:00:58 +0700

    Update uiBenford.R

--- Apps/Benford/uiBenford.R
+++ Apps/Benford/uiBenford.R
@@ -12,7 +12,7 @@ iBenford_tab <- tabItem(
     fileInput("uploadBenford", "Upload a file", accept = c(".csv", ".tsv",".xlsx")),
     uiOutput("varSelectUIBenford"),
     tabBox(
-      id = "Box",
+      id = "Benford",
       title = "Benford Analysis",
       elevation = 2,
       width = 12,
@@ -24,11 +24,14 @@ iBenford_tab <- tabItem(
       selected = "Benford Plot",
       tabPanel(
         "Benford Plot",
-        plotOutput("PlotBenford") %>% withSpinner(color = "#FFEB7A"),
+        plotOutput("PlotBenford") %>% withSpinner(color = "#0dc5c1"),
       ),
       tabPanel(
         "Suspected Records",
-        DTOutput("BenfordResult") %>% withSpinner(color = "#FFEB7A")
+        div(
+          style = "min-height: 400px; overflow-x: auto; max-width: 100%;",
+          DTOutput("BenfordResult") %>% withSpinner(color = "#0dc5c1")
+        )
       )
     )
   )
